cac52f247352284890cd170e6bd91d74a09873b9
Vitor <vitorbastos195@gmail.com>
2025-04-09 12:30:24 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/bastosvitor/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.bastosvitor.workshopmongo.repository;
+
+import com.bastosvitor.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/bastosvitor/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.bastosvitor.workshopmongo.services;
+
+import com.bastosvitor.workshopmongo.domain.User;
+import com.bastosvitor.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+
+}
