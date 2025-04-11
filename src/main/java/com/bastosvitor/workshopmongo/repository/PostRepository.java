ded78b9051cb1283dde72dddbe448fffbf7eb99d
Vitor <vitorbastos195@gmail.com>
2025-04-11 11:28:41 -0300

    Criando entity Post com user aninhado

---
+++ src/main/java/com/bastosvitor/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.bastosvitor.workshopmongo.repository;
+
+import com.bastosvitor.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
